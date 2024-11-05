3a8e11e45ed96139716ca9f14221cc21bc53c617
luizde <luis.delgado.padilla@gmail.com>
2024-11-04 21:22:40 -0500

    rename variable
    rename variable

--- assets/megaman/states/fall.gd
+++ assets/megaman/states/fall.gd
@@ -12,12 +12,8 @@ func physics_process(_delta: float) -> int:
 	else:
 		player.velocity.y += WorldPhysics.gravity
 	
-	player.velocity.x = player.direction * player.is_moving_horizontal * player.SPEED 
-		
-	#player.move_and_collide(Vector2(
-	#			move * player.SPEED * _delta,
-	#			player.gravity * _delta
-	#))
+	player.velocity.x = player.direction * player.is_moving_horizontal * player.speed 
+	
 	#TODO: maybe reconsider and use move_and_collide. The downside is we need to create
 	# 		our own is_on_floor, but we get more control and physics are easily defined here
 	player.move_and_slide()
