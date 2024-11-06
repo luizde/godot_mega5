e47dd71ea10498716752822f8f1337ef1e66e95b
luizde <luis.delgado.padilla@gmail.com>
2024-11-05 21:09:58 -0500

    FX Emmitter reusable
    Create FX Emmitter reusable

---
+++ assets/common/fx/fx_emmitter.gd
@@ -0,0 +1,29 @@
+extends Node
+
+@export var fx: PackedScene
+@export var fx_duration: float
+@onready var fx_duration_timer: Timer = $FxDurationTimer
+
+var _new_fx:Node
+
+func emit_fx(parent_node: Node2D, position: Vector2, move_with_parent: bool = true, flip_h: bool = false, flip_v: bool = false):
+	_new_fx = fx.instantiate()
+	
+	_new_fx.set_position(position)
+	
+	if flip_h:
+		_new_fx.scale.x *= -1
+		
+	if flip_v:
+		_new_fx.scale.y *= -1
+	
+	if !move_with_parent:
+		_new_fx.top_level = true
+		
+	parent_node.add_child(_new_fx)
+	
+	fx_duration_timer.start()
+
+
+func _on_fx_duration_timer_timeout() -> void:
+	_new_fx.queue_free()
