65ead681778342ca655826b0cdb1bb805686fd7a
luizde <luis.delgado.padilla@gmail.com>
2024-11-06 22:22:32 -0500

    Enemy spawner object
    Configurable enemy spawner object

---
+++ assets/enemies/enemy_spawner.gd
@@ -0,0 +1,25 @@
+extends Area2D
+
+@export var enemy_scene: PackedScene
+var enemy_instance
+
+func _ready() -> void:
+	pass
+
+func spawn_enemy() -> void:
+	if enemy_instance == null:
+		enemy_instance = enemy_scene.instantiate()
+		
+		# Use this as I was getting an error "Can't change this state while flushing queries.
+		# Need to read more about this
+		call_deferred("add_child", enemy_instance)
+		#add_child(enemy_instance)
+
+
+
+func _on_area_exited(area: Area2D) -> void:
+	spawn_enemy()
+
+
+func _on_area_entered(area: Area2D) -> void:
+	spawn_enemy()
