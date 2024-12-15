f368308645b267a226a317541f8abd70c699aa6c
luizde <luis.delgado.padilla@gmail.com>
2024-12-14 23:30:26 -0600

    enemy pukapelly behavior
    enemy pukapelly behavior

---
+++ assets/enemies/enemy_pukapelly.gd
@@ -0,0 +1,47 @@
+extends BaseEnemy
+
+@onready var timer_moving: Timer = $TimerMoving
+@onready var timer_stopped: Timer = $TimerStopped
+
+@export var speed: float = 60.0
+
+var _moving:bool = false
+var _move_towards: Vector2
+var _is_active:bool = false
+
+func _ready() -> void:
+	super()
+	timer_stopped.start()
+
+
+func _physics_process(_delta:float) -> void:
+	if _moving and _is_active:
+		#GodotLogger.debug("Pukapelly Moving towards " % _move_towards)
+		position = position.move_toward(_move_towards, speed * _delta)
+		#position.lerp(player.position, 0.012)
+		
+		
+		
+
+
+func _on_timer_stopped_timeout() -> void:
+	_moving = true
+	timer_moving.start()
+	_move_towards = player.position
+	animated_sprite_2d.play("open")
+
+
+func _on_timer_moving_timeout() -> void:
+	_moving = false
+	timer_stopped.start()
+	animated_sprite_2d.play("closed")
+
+
+func _on_detection_range_body_entered(body: Node2D) -> void:
+	if body.is_in_group("player"):
+		_is_active = true
+
+
+func _on_detection_range_body_exited(body: Node2D) -> void:
+	if body.is_in_group("player"):
+		_is_active = false
