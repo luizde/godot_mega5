eac8e2d523022aa2d352cc4e6095b7167b0d050f
luizde <luis.delgado.padilla@gmail.com>
2024-12-27 12:35:03 -0600

    screen shake when hit

--- assets/camera/camera.gd
+++ assets/camera/camera.gd
@@ -2,6 +2,14 @@ extends Camera2D
 
 @onready var player: Player = $"../megaman"
 
+#region Screen Shake vars
+@export var screen_shake_strength: float = 15.0
+## how fast the shake recedes. larger means faster
+@export var screen_shake_fade:float = 8.0
+var _rng: RandomNumberGenerator = RandomNumberGenerator.new()
+var _shake_strength: float = 0.0
+#endregion 
+
 var scroll_speed_multiplier = 3.0
 
 #var y_distance
@@ -10,13 +18,17 @@ var next_position: Vector2
 func _ready() -> void:
 	EventBus.player_enters_room.connect(change_room)
 	next_position = position
-
+	EventBus.enemy_hit_player.connect(_handle_screen_shake)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta: float) -> void:
 	position.x = player.position.x
 	
 	position.y = clampf(position.y + scroll_speed_multiplier, -10000, next_position.y)
+	
+	if _shake_strength > 0.0:
+		_shake_strength = lerpf(_shake_strength, 0, screen_shake_fade * _delta)
+		offset = _random_offset()
 	
 	
 func change_room(_room_number: int, room_pos: Vector2, room_rect: Rect2) -> void:
@@ -26,3 +38,9 @@ func change_room(_room_number: int, room_pos: Vector2, room_rect: Rect2) -> void
 	limit_left = room_rect.position.x + room_pos.x
 	limit_right = (room_rect.position.x + room_pos.x) + room_rect.size.x 
 	
+func _handle_screen_shake(_enemy_name: String, damage_hp: int) -> void:
+	_shake_strength = screen_shake_strength
+
+func _random_offset() -> Vector2:
+	return Vector2( _rng.randf_range(-_shake_strength, _shake_strength),\
+			_rng.randf_range(-_shake_strength, _shake_strength))
